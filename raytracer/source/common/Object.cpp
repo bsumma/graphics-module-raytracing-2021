ac23e1b0aead28cf9dfe1a414d8b5ece16599bc8
bsumma <bsumma@sci.utah.edu>
2020-11-05 11:52:07 -0600

    world - object - world code

--- raytracer/source/common/Object.cpp
+++ raytracer/source/common/Object.cpp
@@ -12,7 +12,39 @@
 /* -------------------------------------------------------------------------- */
 Object::IntersectionValues Sphere::intersect(vec4 p0_w, vec4 V_w){
   IntersectionValues result;
-  //TODO: Ray-sphere setup
+  //ray start in object space
+  vec4 p0_o = INVC*p0_w;
+  //ray direction in object space
+  vec4 V_o  = INVCStar*V_w;
+  double mag = length(V_o);
+  V_o  = normalize(V_o);
+  
+  //Intersect in object space
+  double t_o = raySphereIntersection(p0_o, V_o);
+  if(t_o == std::numeric_limits< double >::infinity()){
+    result.t_w = std::numeric_limits< double >::infinity();
+    return result;
+  }
+
+  //t in world space
+  result.t_w = t_o/mag;
+  
+  //intersection point in object space
+  vec4 P_o = p0_o + t_o*V_o;
+  P_o /= P_o.w;
+  
+  //intersection point in world space
+  result.P_w = p0_w+result.t_w*V_w;
+  result.P_w /= result.P_w.w;
+  
+  //normal in object space
+  vec4 N_o = normalize(vec4(P_o.x,P_o.y,P_o.z, 0.0));
+
+  //normal in world space
+  vec4 N_w  = TRANINVC * N_o;
+  N_w.w = 0;
+  result.N_w = normalize(N_w);
+  
   return result;
 }
 
@@ -28,7 +60,38 @@ double Sphere::raySphereIntersection(vec4 p0, vec4 V, vec4 O, double r){
 /* -------------------------------------------------------------------------- */
 Object::IntersectionValues Square::intersect(vec4 p0_w, vec4 V_w){
   IntersectionValues result;
-  //TODO: Ray-square setup
+  //ray start in object space
+  vec4 p0_o = INVC*p0_w;
+  //ray direction in object space
+  vec4 V_o  = INVCStar*V_w;
+  double mag = length(V_o);
+  V_o  = normalize(V_o);
+  
+  //intersection param in object space
+  double t_o = raySquareIntersection(p0_o, V_o);
+  if(t_o == std::numeric_limits< double >::infinity()){
+    result.t_w = std::numeric_limits< double >::infinity();
+    return result;
+  }
+  
+  //intersection param in world space
+  result.t_w = t_o/mag;
+  
+  //intersection point in object space
+  vec4 P_o = p0_o+t_o*V_o;
+  P_o /= P_o.w;
+  
+  //intersection point in world space
+  result.P_w = p0_w+result.t_w*V_w;
+  result.P_w /= result.P_w.w;
+  
+  //normal in object space
+  vec4 N_o = vec4(0.0,0.0,1.0,0.0);
+  
+  //normal in world space
+  vec4 N_w  = TRANINVC*N_o;
+  N_w.w = 0;
+  result.N_w = normalize(N_w);
   
   return result;
 }
